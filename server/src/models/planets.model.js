70df14e98c4181f20c67d07c390ce724f9b7ddc8
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-12-18 19:29:12 -0300

    feat: add script to execute paralell client and server

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,35 @@
-const planets = []
+const { parse } = require('csv-parse') 
+const fs = require('fs');
+const path = require('path')
 
-module.exports = planets
+const habitablePlanets = []
+
+const isHabitablePlanet = (planet) => planet['koi_disposition'] === 'CONFIRMED'
+  && planet['koi_insol'] > 0.33 && planet['koi_insol'] < 1.11
+  && planet['koi_prad'] < 1.6
+
+function loadPlanets() { 
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, '../data/kapler_data_12_21.csv'))
+      .pipe(parse({
+        comment: '#',
+        columns: true
+      }))
+      .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data)
+        }
+      })
+      .on('error', (error) => {
+        reject(error)
+      })
+      .on('end', () => { 
+        resolve()
+      })
+  })
+}
+
+module.exports = {
+  loadPlanets,
+  planets: habitablePlanets
+}
--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,16 @@
 const http = require('http')
 const app = require('./app')
+const { loadPlanets } = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app)
 
-server.listen(PORT, () => {
-  console.log(`API Nasa runing at port: ${PORT}`)
-})
+async function startSever() {
+  await loadPlanets()
+  server.listen(PORT, () => {
+    console.log(`API Nasa runing at port: ${PORT}`)
+  })
+}
+
+startSever()
+
